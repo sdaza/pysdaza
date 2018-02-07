0a1c0fce86ad30000071aa6e9a106681a7b4b7bd
Sebastian <sebastian.daza@gmail.com>
2018-02-07 16:09:51 -0600

    add documentation

--- pysdaza/functions.py
+++ pysdaza/functions.py
@@ -4,12 +4,29 @@ import glob
 
 # read files
 def read_files(dir):
+    '''Read several files using pandas and regular expressions
+
+    Args:
+        dir: Text or regular expression. E.g., 'myfiles/*.csv'.
+
+    Returns:
+        Pandas dataframe.
+
+    '''
     filenames = glob.glob(dir)
     return pandas.concat([pandas.read_csv(i) for i in filenames],
                      ignore_index=True)
 
 # missing data
-def count_missing(df):
+def prop_missing(df):
+    '''Proportion of missing cases for Pandas dataframe
+
+    Args:
+        df: Dataframe.
+    Returns:
+        Pandas series with proportion of missing cases.
+
+    '''
     m = df.isnull().sum()/len(df)
     m = m[m>0.00]
     if ( len(m) > 0):
@@ -19,6 +36,16 @@ def count_missing(df):
 
 # impute values
 def impute_values(data, group_vars):
+    '''Simple imputation of missing values by group.
+
+    Args:
+        data: Dataframe
+        group_vars: List of variables to group by.
+    Returns:
+        Dataframe with imputed data.
+
+    '''
+
     df = data.copy()
     group= df.groupby(group_vars)
 
@@ -42,6 +69,16 @@ def impute_values(data, group_vars):
 
 # transform variables
 def transform_variables(data, variables, kind='log'):
+    '''Transformation of variable using pandas
+
+    Args:
+        data: Dataframe.
+        variables: List of variables to transform.
+        kind: Type of tranformation. Default is 'log' (natural logarithm). 'z' means z-score.
+
+    Returns:
+        Dataframe with new transform variables (prefix log and z). 
+    '''
     d = data.copy()
     for name in variables:
         if kind=='log':
